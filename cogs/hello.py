f8fa636981c44927b1ea83a0f317fb0cd3b2f362
r3a10god <zenithtaiga@gmail.com>
2021-04-16 02:09:07 +0530

    "nothing"

--- cogs/hello.py
+++ cogs/hello.py
@@ -9,17 +9,24 @@ async def hello(ctx):
 class interacting(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.on_member_join_msg = "G'day! If you leave I'll kill you"
+        self.on_member_remove_msg = "And there they go. It\'s all fun and games until Blaze bans you."
     
     @commands.Cog.listener()
-    async def on_member_join(self, ctx):
-        msg = "G'day! If you leave I'll kill you"
-        await ctx.send(msg)
+    async def on_member_join(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+            await channel.send(on_member_join_msg)
+        else:
+            pass # send msg to admins to create a sys chan
     
     @commands.Cog.listener()
-    async def on_member_remove(self, ctx):
-        msg = "And there they go. It\'s all fun and games until Blaze bans you."
-        await ctx.send(msg)
-
+    async def on_member_remove(self, member):
+        channel = member.guild.system_channel
+        if channel is not None:
+                await channel.send(on_member_remove_msg)
+        else:
+            pass # send msg to admins to create a sys chan
 
 
 def setup(bot):
