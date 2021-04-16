59e002439c7d78c87d55b891367c7c644c4cda22
r3a10god <zenithtaiga@gmail.com>
2021-04-17 00:05:47 +0530

    nothing

--- main.py
+++ main.py
@@ -25,10 +25,8 @@ for extension in cog_extensions:
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}\n{bot.user.id}\n------"
-          )  # bot.user is the 'bot'
-    await bot.change_presence(activity=discord.Game("gghelp")
-                              )  # will display as "playing !help"
+    print(f"Logged in as {bot.user}\n{bot.user.id}\n------")  # bot.user is the 'bot'
+    await bot.change_presence(activity=discord.Game("gghelp"))  # will display as "playing !help"
 
 
 @bot.command()
@@ -36,19 +34,9 @@ async def info(ctx):
     """Gives information on the bot"""
     embed = discord.Embed(colour=discord.Colour.purple())
     embed.set_author(name="Info")
-    embed.add_field(
-        name="What is it?",
-        value=
-        "A discord bot made using python for helping you keep track of your goals",
-        inline=False)
-    embed.add_field(name="Creator",
-                    value="Made by \'Hemant\' aka \'LaughingOutClouds\'",
-                    inline=False)
-    embed.add_field(
-        name="Source code",
-        value=
-        "I\'m not sharing =_=\nYou can join the server tho:\nhttps://discord.gg/Gu4mVGhwWJ",
-        inline=False)
+    embed.add_field(name="What is it?", value="A discord bot made using python for helping you keep track of your goals", inline=False)
+    embed.add_field(name="Creator", value="Made by \'Hemant\' aka \'LaughingOutClouds\'", inline=False)
+    embed.add_field(name="Source code", value="I\'m not sharing =_=\nYou can join the server tho:\nhttps://discord.gg/Gu4mVGhwWJ", inline=False)
     await ctx.send(embed=embed)
 
 
@@ -56,21 +44,14 @@ async def info(ctx):
 async def all(ctx):
     """Shows list of all available commands"""
     embed = discord.Embed(colour=discord.Colour.red())
-    embed.set_author(
-        name=
-        "All commands of The Wise Owl\ngghelp <command_name> for more info on particular commands."
-    )
+    embed.set_author(name="All commands of The Wise Owl\ngghelp <command_name> for more info on particular commands.")
 
     command_list = bot.commands
 
     # Adding all the commands of the bot into the embed obj
     for bot_command in command_list:
-        embed.add_field(name=f'{bot_command}',
-                        value=f"{bot.command_prefix}{bot_command}",
-                        inline=False)
-    embed.add_field(name='End',
-                    value='We\'ll be coming with new features soon',
-                    inline=False)
+        embed.add_field(name=f'{bot_command}', value=f"{bot.command_prefix}{bot_command}", inline=False)
+    embed.add_field(name='End', value='We\'ll be coming with new features soon', inline=False)
 
     await ctx.send(embed=embed)
 
