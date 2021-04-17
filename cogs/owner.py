a6ad3d6ecff821ef1852a217f2e1febcc986e568
r3a10god <zenithtaiga@gmail.com>
2021-04-17 13:13:44 +0530

    added owner.py for commands that can be executed by an owner only

---
+++ cogs/owner.py
@@ -0,0 +1,51 @@
+from discord.ext import commands
+
+
+class OwnerCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+    
+    # hidden: the command won't show in the default help command
+    # is_owner means only the bot owner (or a team which owns the bot) will be able to use this command
+    @commands.command(name='load', hidden=True)
+    @commands.is_owner()
+    async def cog_load(self, ctx, *, cog: str):
+        """Command to load a module.
+        Use the dot path in this one.
+        e.g: cogs.owner"""
+
+        try:
+            self.bot.load_extension(cog)
+        except Exception as e:
+            await ctx.send(f"**`ERORR:`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"{cog} loaded successfully")
+
+
+    @commands.command(name='unload', hidden=True)
+    @commands.is_owner()
+    async def cog_unload(self, ctx, *, cog: str):
+        """Command to unload a module.
+        Use the dot path."""
+
+        try:
+            self.bot.unload_extension(cog)
+        except Exception as e:
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"{cog} unloaded successfully")
+    
+
+    @commands.command(name='reload', hidden=True)
+    @commands.is_owner()
+    async def cog_reload(self, ctx, *, cog: str):
+        """Command to reload a module.
+        Use the dot path."""
+
+        try:
+            self.bot.reload_extension(cog)
+        except Exception as e:
+            await ctx.send(f"**`ERROR:`** {type(e).__name__} - {e}")
+        else:
+            await ctx.send(f"{cog} reloaded successfully")
