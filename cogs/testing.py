5a323cc75d845ec0b73bc1d9d05b3607e5da434c
r3a10god <zenithtaiga@gmail.com>
2021-04-17 15:39:01 +0530

    nothing

--- cogs/testing.py
+++ cogs/testing.py
@@ -1,4 +1,3 @@
-import discord
 from discord.ext import commands
 
 
@@ -12,100 +11,6 @@ class Misc(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-    
-    
-    @commands.command()
-    async def all(self, ctx):
-        """Shows list of all available commands"""
-        embed = discord.Embed(colour=discord.Colour.red())
-        embed.set_author(name="All commands of The Wise Owl\ngghelp <command_name> for more info on particular commands.")
-
-        command_list = self.bot.commands
-
-        # Adding all the commands of the bot into the embed obj
-        for bot_command in command_list:
-            embed.add_field(name=f'{bot_command}', value=f"{bot.command_prefix}{bot_command}", inline=False)
-        embed.add_field(name='End', value='We\'ll be coming with new features soon', inline=False)
-
-        await ctx.send(embed=embed)
-
-
-    """Poll voting system."""
-
-    @commands.command()
-    @commands.guild_only()
-    async def poll(self, ctx, *, question):
-        """Interactively creates a poll with the following question.
-
-        To vote, use reactions!
-        """
-
-        # a list of messages to delete when we're all done
-        messages = [ctx.message]
-        answers = []
-
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel and len(m.content) <= 100
-
-        for i in range(20):
-            messages.append(await ctx.send(f'Say poll option or {ctx.prefix}cancel to publish poll.'))
-
-            try:
-                entry = await self.bot.wait_for('message', check=check, timeout=60.0)
-            except asyncio.TimeoutError:
-                break
-
-            messages.append(entry)
-
-            if entry.clean_content.startswith(f'{ctx.prefix}cancel'):
-                break
-
-            answers.append((to_emoji(i), entry.clean_content))
-
-        try:
-            await ctx.channel.delete_messages(messages)
-        except:
-            pass  # exception handling at its best
-
-        answer = '\n'.join(f'{keycap}: {content}' for keycap, content in answers)
-        actual_poll = await ctx.send(f'{ctx.author} asks: {question}\n\n{answer}')
-        for emoji, _ in answers:
-            await actual_poll.add_reaction(emoji)
-
-    @poll.error
-    async def poll_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArgument):
-            return await ctx.send('Missing the question.')
-
-    @commands.command()
-    @commands.guild_only()
-    async def quickpoll(self, ctx, *questions_and_choices: str):
-        """Makes a poll quickly.
-
-        The first argument is the question and the rest are the choices.
-        """
-
-        if len(questions_and_choices) < 3:
-            return await ctx.send('Need at least 1 question with 2 choices.')
-        elif len(questions_and_choices) > 21:
-            return await ctx.send('You can only have up to 20 choices.')
-
-        perms = ctx.channel.permissions_for(ctx.me)
-        if not (perms.read_message_history or perms.add_reactions):
-            return await ctx.send('Need Read Message History and Add Reactions permissions.')
-
-        question = questions_and_choices[0]
-        choices = [(to_emoji(e), v) for e, v in enumerate(questions_and_choices[1:])]
-
-        try:
-            await ctx.message.delete()
-        except:
-            pass
-
-        body = "\n".join(f"{key}: {c}" for key, c in choices)
-        poll = await ctx.send(f'{ctx.author} asks: {question}\n\n{body}')
-        for emoji, _ in choices:
-            await poll.add_reaction(emoji)
 
 
 def setup(bot):
